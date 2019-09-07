5de6bb7de68989b2289238eb315e3fafc4baceb2
arl5705 <arl5705@rit.edu>
2019-09-07 13:00:39 -0400

    Program works with correct inputs

--- lab2.py
+++ lab2.py
@@ -1,38 +1,64 @@
 """
 Use recursion to print nested squares/diamonds
 Precondition: Turtle starts facing east in bottom left
+Postcondition: Correct shape/colors are drawn, turtle is in start position
 """
 
 import turtle
 
-"""
-Does the actual drawing.
-Draws layered squares, which contain
-Diamonds arranged east-west, which contain
-Squares, arranged NW-SE, etc.
-If depth is odd, shapes will be blue
-If depth is even, shapes will be orange
-"""
-def draw_squares(depth):
-    if depth % 2 == 1:
-        turtle.color('blue')
-    else:
-        turtle.color('orange')
+def set_color(depth):
+    """
+    Sets the color of the squares
+    If depth is even, shapes will be orange
+    If depth is odd, shapes will be blue
+    """
+    colors = ['orange', 'blue']
+    turtle.color(colors[depth % 2])
 
+
+def recur(length, depth):
+    """
+    Does the actual drawing.
+    Draws layered squares, which contain
+    Diamonds arranged east-west, which contain
+    Squares arranged NW-SE,
+    ad infinitum
+
+    Pre: nothing drawn, turtle in start position
+    Post: squares drawn, turtle in start position
+    """
     if depth > 0:
-        pass
+        for i in range(2):
+            set_color(depth)
+            turtle.forward(length)
+            turtle.left(90)
+            turtle.forward(length)
+            turtle.backward(length/2)
+            turtle.left(45)
+            recur(length / (2 ** 1.5), depth - 1)
+            turtle.right(45)
+            turtle.up()
+            turtle.forward(length/2)
+            turtle.down()
+            turtle.left(90)
 
-"""Sets up the turtle to be in the bottom left"""
 def init():
+    """
+    Sets up the turtle to be in the bottom left
+
+    Pre: Turtle is in default position (0,0)
+    Post: Turtle is in bottom left of screen
+    """
     turtle.up()
-    turtle.setpos(-240,-220)
+    turtle.setpos(-200,-200)
     turtle.down()
 
-"""Gets input and calls init() and draw_squares()"""
 def main():
+    """Gets input and calls init() and recur()"""
     init()
     depth = int(input('How many layers of recursion?\n'))
-    draw_squares(depth)
+    length = 400
+    recur(length, depth)
     turtle.mainloop()
 
 if __name__ == '__main__':
